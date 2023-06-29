0227bbf9712fe1e47e1f5e055b94d549304a6447
Omar Franco <fofe2803@gmail.com>
2023-06-29 02:14:54 -0600

    feat (service): [infrastructure] create the dependencies file to connect different the layers

---
+++ src/services/infrastructure/dependencies.ts
@@ -0,0 +1,31 @@
+import { CreateServiceUseCase } from "../application/CreateServiceUseCase";
+import { DeleteServiceUseCase } from "../application/DeleteServiceUseCase";
+import { FindAllServiceUseCase } from "../application/FindAllServiceUseCase";
+import { FindByIdServiceUseCase } from "../application/FindByIdServiceUseCase";
+import { UpdateServiceUseCase } from "../application/UpdateServiceUseCase";
+
+import { CreateServiceController } from "./controllers/CreateServiceController";
+import { DeleteServiceController } from "./controllers/DeleteServiceController";
+import { FindAllServiceController } from "./controllers/FindAllServiceController";
+import { FindByIdServiceController } from "./controllers/FindByIdServiceController";
+import { UpdateServiceController } from "./controllers/UpdateServiceController";
+
+import { ServiceRepository } from "./implementation/ServiceRepository";
+
+const serviceRepository = new ServiceRepository();
+
+const createServiceUseCase = new CreateServiceUseCase(serviceRepository);
+export const createServiceController = new CreateServiceController(createServiceUseCase);
+
+const findByIdServiceUseCase = new FindByIdServiceUseCase(serviceRepository);
+export const findByIdServiceController = new FindByIdServiceController(findByIdServiceUseCase);
+
+const deleteServiceUseCase = new DeleteServiceUseCase(serviceRepository);
+export const deleteServiceController = new DeleteServiceController(deleteServiceUseCase, findByIdServiceUseCase);
+
+const findAllServiceUseCase = new FindAllServiceUseCase(serviceRepository);
+export const findAllServiceController = new FindAllServiceController(findAllServiceUseCase);
+
+const updateServiceUseCase = new UpdateServiceUseCase(serviceRepository);
+export const updateServiceController = new UpdateServiceController(updateServiceUseCase, findByIdServiceUseCase);
+
