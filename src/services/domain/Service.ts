0b37b92eedfff4d0b776ff5e5f47573d8c8ec163
Omar Franco <fofe2803@gmail.com>
2023-06-29 01:58:37 -0600

    feat (service): [domain] create the model of my new services

---
+++ src/services/domain/Service.ts
@@ -0,0 +1,28 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Service extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    serviceId!: number;
+
+    @Column()
+    providerId!: number;
+
+    @Column()
+    name!: string;
+
+    @Column()
+    description!: string;
+
+    @Column('varchar', {array: true, nullable: true})
+    tags!: string[] | null;
+
+    @Column('simple-array')
+    urlImages!: string[];
+
+    @Column({ default: () => "CURRENT_TIMESTAMP" })
+    createdAt!: Date;
+
+    @Column({ default: () => "CURRENT_TIMESTAMP", onUpdate: "CURRENT_TIMESTAMP" })
+    updatedAt!: Date;
+}
