165809cedac38b993a964f3fcab860a6a3725500
Omar Franco <fofe2803@gmail.com>
2023-07-04 23:05:06 -0600

    chore: remove useless comments and code

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,8 @@
 import express from "express";
-// import aws from "aws-sdk";
-
 import { serviceRouter } from "./services/infrastructure/ServiceRouter";
 import path from "path";
 
 const app = express();
-
-// aws.config.update({
-//   accessKeyId: process.env.AccessKeyId,
-//   secretAccessKey: process.env.SecretAccessKey,
-// });
-
-// const s3 = new aws.S3();
-
-// app.get("/uploads/:filename", (req, res) => {
-//   const filename = req.params.filename;
-
-//   const params = {
-//     Bucket: process.env.BUCKET_NAME!,
-//     Key: filename,
-//   };
-
-//   // Obtener el objeto desde S3
-//   s3.getObject(params, (err, data) => {
-//     if (err) {
-//       console.error(err);
-//       res.status(500).send("Error al obtener la imagen desde S3");
-//     } else {
-//       // Configurar los encabezados de respuesta para la imagen
-//       res.setHeader("Content-Length", data.ContentLength!);
-//       res.setHeader("Content-Type", data.ContentType!);
-//       res.send(data.Body);
-//     }
-//   });
-// });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- src/services/infrastructure/ServiceRouter.ts
+++ src/services/infrastructure/ServiceRouter.ts
@@ -1,10 +1,5 @@
 import express from 'express';
 import multer from 'multer';
-// import { S3Client } from "@aws-sdk/client-s3";
-import dotenv from "dotenv";
-// import multerS3 from 'multer-s3';
-
-dotenv.config(); // Cargar variables de entorno desde el archivo .env
 
 import {
   createServiceController,
@@ -18,23 +13,6 @@ import path from 'path';
 
 export const serviceRouter = express.Router();
 
-// const s3Client  = new S3Client({
-//   region: process.env.REGION_S3, // Ejemplo: "us-east-1"
-//   credentials:{
-//     accessKeyId: process.env.AccessKeyId!,
-//     secretAccessKey: process.env.SecretAccessKey!,
-//   },
-// });
-// export const uploadS3 = multer({
-//   storage: multerS3({
-//     s3: s3Client,
-//     bucket: process.env.BUCKET_NAME!,
-//     key: function (req, file, cb) {
-//       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-//       cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split(".").pop());
-//     },
-//   }),
-// });
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -49,10 +27,8 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 serviceRouter.post('/',upload.array("images"), createServiceController.run.bind(createServiceController));
-// serviceRouter.post('/',uploadS3.array("images"), createServiceController.run.bind(createServiceController));
 serviceRouter.delete('/:id', deleteServiceController.run.bind(deleteServiceController));
 serviceRouter.patch('/:id', upload.array("images"), updateServiceController.run.bind(updateServiceController));
-// serviceRouter.patch('/:id', uploadS3.array("images"), updateServiceController.run.bind(updateServiceController));
 serviceRouter.get('/', findAllServiceController.run.bind(findAllServiceController));
 serviceRouter.get('/:id', findByIdServiceController.run.bind(findByIdServiceController));
 serviceRouter.get('/provider/:id', findByProviderIdController.run.bind(findByProviderIdController));
