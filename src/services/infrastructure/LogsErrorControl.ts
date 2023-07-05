c47ca414d4362de09938e3439c12773602c24b18
Omar Franco <fofe2803@gmail.com>
2023-07-04 23:08:31 -0600

    feat: save error log file function

---
+++ src/services/infrastructure/LogsErrorControl.ts
@@ -0,0 +1,19 @@
+import fs from 'fs';
+ function saveErrorToLogFile(error: unknown, errorLine: string) {
+  try {
+    const currentDate = new Date();
+    const formattedFullDate = currentDate.toLocaleString();
+    const formattedDate = `${currentDate.getDate()}-${currentDate.getMonth() + 1}-${currentDate.getFullYear()}`;
+    const logFileName = `error-logs-${formattedDate}.txt`;
+    const logFilePath = `src/logs/${logFileName}`;
+    const logMessage = `${formattedFullDate}    ${error}    ${errorLine}\n`;
+     if (!fs.existsSync(logFilePath)) {
+      fs.writeFileSync(logFilePath, logMessage);
+    } else {
+      fs.appendFileSync(logFilePath, logMessage);
+    }
+  } catch (error) {
+    console.error('Error saving error to log file:', error);
+  }
+}
+ export default saveErrorToLogFile;
